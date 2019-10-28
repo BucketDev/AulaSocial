6f205461c403cd8a7a8c8ea43fa8446f5b3c0e16
vanfigo <rodrigo.loyola@live.com>
2019-10-28 16:24:54 -0600

    Login component html y css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoginModule } from "./components/login/login.module";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AngularMaterialModule } from "../angular-material.module";
+
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    MatToolbarModule,
+    
+  ],
+  declarations: [
+    LoginComponent
+  ]
+})
+export class LoginModule { }

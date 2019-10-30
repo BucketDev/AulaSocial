b915b7362c9cddb2555b193520036cf227a11bf9
vanfigo <rodrigo.loyola@live.com>
2019-10-30 16:50:33 -0600

    hide admin menu from view and navigation

--- src/app/providers/auth/admin-guard.service.ts
+++ src/app/providers/auth/admin-guard.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { FireAuthService } from './fire-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuardService implements CanActivate {
   
+  constructor(private fireAuth: FireAuthService,
+              private router: Router) { }
+
   canActivate() {
-    return true;
+    if (!this.fireAuth.user.admin)
+      this.router.navigate(['/dashboard']);
+    return this.fireAuth.user.admin;
   }
 
-  constructor() { }
 }
